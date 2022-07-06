b27b5eeba6e93b37640af6f6de9b101b7beacf25
EmpAsh938 <ashokthapa938@gmail.com>
2022-07-06 13:54:35 +0545

    changed fixed

--- stock-photo/src/index.js
+++ stock-photo/src/index.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-=======
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
@@ -26,4 +12,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
->>>>>>> f364450 (updated projects)
